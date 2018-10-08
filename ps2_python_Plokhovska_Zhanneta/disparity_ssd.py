dac506337876013b414f6200b6597f235a364aab
Zhanneta Plokhovska <zhp3@pitt.edu>
2018-10-08 10:02:41 -0400

    Stereo Vision - Draft 2

--- ps2_python_Plokhovska_Zhanneta/disparity_ssd.py
+++ ps2_python_Plokhovska_Zhanneta/disparity_ssd.py
@@ -9,29 +9,36 @@ import math
 ##################################################
 def disparity_ssd(L, R):
 	rows, columns = L.shape # Assuming L and R are same size
-	displ_t = range(-4, 5, 1) # Displacement for template (size = 9x9)
+	displ_t = range(-3, 3, 1) # Displacement for template (size = 7x7)
 	
 	# Initialize disparity image - same size as L and R
 	D = np.zeros((rows, columns), np.uint8)
 	
+	#print "\n------L-------\n"
+	#print "{} {} {} {}".format(L[0][0], L[0][1], L[0][2], L[0][3])
+	#print "\n------R-------\n"
+	#print "{} {} {} {}".format(R[0][0], R[0][1], R[0][2], R[0][3])
+	#print "\n--------------\n"	
+	
 	# For every pixel in image L
 	for i in range(rows):
 		for j in range(columns):
-			pixel_ssd = [0] * columns # Store ssd for every pixel on epipolar line
+			pixel_ssd = [2] * columns # Store ssd for every pixel on epipolar line
 			# For every pixel in epipolar line in R
 			for k in range(columns): # row stays same = i
-				# For every pixel in template
+				# Calculate ssd for pixel using template from L
 				for row_t in displ_t:
 					for column_t in displ_t:
 						L_R_row = i + row_t
 						L_column = j + column_t
 						R_column = k + column_t
 						if ((L_R_row >= 0) & (L_R_row < rows) & (L_column >= 0) & (L_column < columns) & (R_column >= 0) & (R_column < columns)):
-							pixel_ssd[k] += math.pow(L[i+row_t][j+column_t] - R[i+row_t][k+column_t], 2)			
+							pixel_ssd[k] = pixel_ssd[k] + math.pow(L[L_R_row][L_column] - R[L_R_row][R_column], 2)
+							#print "{} += math.pow({} - {}, 2)".format(pixel_ssd[k], L[L_R_row][L_column], R[L_R_row][R_column])
+				#print pixel_ssd
 			# Pick best match
 			pixel_index_min_ssd = pixel_ssd.index(min(pixel_ssd))
-			D[i][pixel_index_min_ssd] = 2
-	
-	print D
+			#print "pixel at [{}, {}] -> {}".format(i, j, pixel_index_min_ssd)
+			D[i][pixel_index_min_ssd] = min(pixel_ssd)
 	
 	return D
--- ps2_python_Plokhovska_Zhanneta/ps2.py
+++ ps2_python_Plokhovska_Zhanneta/ps2.py
@@ -1,23 +1,77 @@
-# ps2
+# Public libraries
 import os
 import numpy as np
 import cv2
 
+# Private libraries
 from disparity_ssd import disparity_ssd
 
-## 1-a
+if 1 == 0:
+	## 1-a
+	# Read images
+	L = cv2.imread(os.path.join('input', 'pair0-L.png'), 0) * (1.0 / 255.0)  # grayscale, [0, 1]
+	R = cv2.imread(os.path.join('input', 'pair0-R.png'), 0) * (1.0 / 255.0)
+	rows, columns = L.shape
+	# Compute disparity (using method disparity_ssd defined in disparity_ssd.py)
+	D_L = disparity_ssd(L, R)
+	D_R = disparity_ssd(R, L)
+	# Save output images (D_L as output/ps2-1-a-1.png and D_R as output/ps2-1-a-2.png)
+	# Note: They may need to be scaled/shifted before saving to show results properly
+	D_L_equ = cv2.equalizeHist(D_L)
+	cv2.imwrite('output/ps2-1-a-1.png', D_L_equ)
+	D_R_equ = cv2.equalizeHist(D_R)
+	cv2.imwrite('output/ps2-1-a-2.png', D_R_equ)
+
+	## 2-a
+	# Read images
+	L = cv2.imread('input/pair1-L.png', 0)
+	R = cv2.imread('input/pair1-R.png', 0)
+	rows, columns = L.shape
+	# Compute disparity (using method disparity_ssd defined in disparity_ssd.py)
+	D_L = disparity_ssd(L, R)
+	D_R = disparity_ssd(R, L)
+	# Save output images (D_L as output/ps2-1-a-1.png and D_R as output/ps2-1-a-2.png)
+	# Note: They may need to be scaled/shifted before saving to show results properly
+	D_L_equ = cv2.equalizeHist(D_L)
+	cv2.imwrite('output/ps2-2-a-1.png', D_L_equ)
+	D_R_equ = cv2.equalizeHist(D_R)
+	cv2.imwrite('output/ps2-2-a-2.png', D_R_equ)
+
+## 3-a
+# Read images
+L = cv2.imread('input/pair1-L.png', 0)
+R = cv2.imread('input/pair1-R.png', 0)
+rows, columns = L.shape
+# Add gaussian noise to L
+mu = 0
+sigma = 3
+noise = np.random.normal(mu, sigma, rows*columns)
+L = noise.reshape(rows, columns) + L
+cv2.imwrite('output/noisy.png', L)
+# Compute disparity (using method disparity_ssd defined in disparity_ssd.py)
+D_L = disparity_ssd(L, R)
+D_R = disparity_ssd(R, L)
+# Save output images (D_L as output/ps2-1-a-1.png and D_R as output/ps2-1-a-2.png)
+# Note: They may need to be scaled/shifted before saving to show results properly
+D_L_equ = cv2.equalizeHist(D_L)
+cv2.imwrite('output/ps2-3-a-1.png', D_L_equ)
+D_R_equ = cv2.equalizeHist(D_R)
+cv2.imwrite('output/ps2-3-a-2.png', D_R_equ)
+
+## 3-b
 # Read images
 L = cv2.imread(os.path.join('input', 'pair0-L.png'), 0) * (1.0 / 255.0)  # grayscale, [0, 1]
 R = cv2.imread(os.path.join('input', 'pair0-R.png'), 0) * (1.0 / 255.0)
-
+rows, columns = L.shape
+# Add brightness to L
+L = L * 1.1 # 10%
+cv2.imwrite('output/bright.png', L)
 # Compute disparity (using method disparity_ssd defined in disparity_ssd.py)
 D_L = disparity_ssd(L, R)
-cv2.imwrite('output/ps2-1-a-1.png', D_L)
 D_R = disparity_ssd(R, L)
-cv2.imwrite('output/ps2-1-a-2.png', D_R)
-
-# TODO: Save output images (D_L as output/ps2-1-a-1.png and D_R as output/ps2-1-a-2.png)
+# Save output images (D_L as output/ps2-1-a-1.png and D_R as output/ps2-1-a-2.png)
 # Note: They may need to be scaled/shifted before saving to show results properly
-
-
-# TODO: Rest of your code here
+D_L_equ = cv2.equalizeHist(D_L)
+cv2.imwrite('output/ps2-3-b-1.png', D_L_equ)
+D_R_equ = cv2.equalizeHist(D_R)
+cv2.imwrite('output/ps2-3-b-2.png', D_R_equ)
