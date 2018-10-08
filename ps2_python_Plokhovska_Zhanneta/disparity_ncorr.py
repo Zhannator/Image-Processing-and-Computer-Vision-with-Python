435cf5180d44e5c60bacc193b32dd90942f093ab
Zhanneta Plokhovska <zhp3@pitt.edu>
2018-10-08 13:49:23 -0400

    Stereo Vision - Draft 3

--- ps2_python_Plokhovska_Zhanneta/disparity_ncorr.py
+++ ps2_python_Plokhovska_Zhanneta/disparity_ncorr.py
@@ -1,11 +1,47 @@
+import numpy as np
+import math
+from progressbar import ProgressBar
+
+##################################################
+# disparity_ncorr
+# Returns disparity image D(y,x) 
+# such that L(y,x) = R(y,x+D(y,x)) when matching
+# from left (L) to right (R)
+##################################################
 def disparity_ncorr(L, R):
-    """Compute disparity map D(y, x) such that: L(y, x) = R(y, x + D(y, x))
-    
-    Params:
-    L: Grayscale left image
-    R: Grayscale right image, same size as L
-
-    Returns: Disparity map, same size as L, R
-    """
-
-    # TODO: Your code here
+	pbar = ProgressBar()
+	rows, columns = L.shape # Assuming L and R are same size
+	displ_t = range(-3, 3, 1) # Displacement for template (size = 7x7)
+	
+	# Initialize disparity image - same size as L and R
+	D = np.zeros((rows, columns), np.uint8)
+	
+	# For every pixel in image L
+	for i in pbar(range(rows)):
+		for j in range(columns):
+			rtx = [0] * columns # Store dot for every pixel on epipolar line
+			rxx = [0] * columns # Help normalization
+			rtt = [0] * columns # Help normalization
+			# For every pixel in epipolar line in R
+			for k in range(columns): # row stays same = i
+				# Calculate dot for pixel using template from L
+				for row_t in displ_t:
+					for column_t in displ_t:
+						L_R_row = i + row_t
+						L_column = j + column_t
+						R_column = k + column_t
+						if ((L_R_row >= 0) & (L_R_row < rows) & (L_column >= 0) & (L_column < columns) & (R_column >= 0) & (R_column < columns)):
+							rtx[k] = rtx[k] + (L[L_R_row][L_column] * R[L_R_row][R_column])
+						# rxx
+						if ((L_R_row >= 0) & (L_R_row < rows) & (R_column >= 0) & (R_column < columns)):
+							rxx[k] = rxx[k] + (R[L_R_row][R_column] * R[L_R_row][R_column])
+						# rtt
+						if ((L_R_row >= 0) & (L_R_row < rows) & (L_column >= 0) & (L_column < columns)):
+							rtt[k] = rtt[k] + (L[L_R_row][L_column] * L[L_R_row][L_column])
+				# Normalize
+				rtx[k] = rtx[k] / math.sqrt(rxx[k] * rtt[k])
+			# Pick best match
+			pixel_index_min_dot = rtx.index(max(rtx)) # Max value when pixels match
+			D[i][pixel_index_min_dot] = max(rtx)
+	
+	return D
--- ps2_python_Plokhovska_Zhanneta/disparity_ssd.py
+++ ps2_python_Plokhovska_Zhanneta/disparity_ssd.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from progressbar import ProgressBar
 
 ##################################################
 # disparity_ssd
@@ -8,22 +9,17 @@ import math
 # from left (L) to right (R)
 ##################################################
 def disparity_ssd(L, R):
+	pbar = ProgressBar()
 	rows, columns = L.shape # Assuming L and R are same size
 	displ_t = range(-3, 3, 1) # Displacement for template (size = 7x7)
 	
 	# Initialize disparity image - same size as L and R
 	D = np.zeros((rows, columns), np.uint8)
 	
-	#print "\n------L-------\n"
-	#print "{} {} {} {}".format(L[0][0], L[0][1], L[0][2], L[0][3])
-	#print "\n------R-------\n"
-	#print "{} {} {} {}".format(R[0][0], R[0][1], R[0][2], R[0][3])
-	#print "\n--------------\n"	
-	
 	# For every pixel in image L
-	for i in range(rows):
+	for i in pbar(range(rows)):
 		for j in range(columns):
-			pixel_ssd = [2] * columns # Store ssd for every pixel on epipolar line
+			pixel_ssd = [0] * columns # Store ssd for every pixel on epipolar line
 			# For every pixel in epipolar line in R
 			for k in range(columns): # row stays same = i
 				# Calculate ssd for pixel using template from L
@@ -34,11 +30,8 @@ def disparity_ssd(L, R):
 						R_column = k + column_t
 						if ((L_R_row >= 0) & (L_R_row < rows) & (L_column >= 0) & (L_column < columns) & (R_column >= 0) & (R_column < columns)):
 							pixel_ssd[k] = pixel_ssd[k] + math.pow(L[L_R_row][L_column] - R[L_R_row][R_column], 2)
-							#print "{} += math.pow({} - {}, 2)".format(pixel_ssd[k], L[L_R_row][L_column], R[L_R_row][R_column])
-				#print pixel_ssd
 			# Pick best match
 			pixel_index_min_ssd = pixel_ssd.index(min(pixel_ssd))
-			#print "pixel at [{}, {}] -> {}".format(i, j, pixel_index_min_ssd)
 			D[i][pixel_index_min_ssd] = min(pixel_ssd)
 	
 	return D
