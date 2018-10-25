28fa51f78fa87ed265f7f28b2f7eb288fed985ef
Zhanneta Plokhovska <zhp3@pitt.edu>
2018-10-25 19:09:09 -0400

    Caliberation - Draft 2

--- ps3_python_Plokhovska_Zhanneta/ps3.py
+++ ps3_python_Plokhovska_Zhanneta/ps3.py
@@ -23,12 +23,13 @@ def svd(points_2d, points_3d):
 	
 	# Find eigenvectors of AtA
 	eigenvalues, eigenvectors = np.linalg.eig(AtA)
-	print eigenvalues
-	print eigenvectors
 	
 	# Find eigenvector with smallest eigenvalue
 	# column eigenvectors[:,i] is the eigenvector corresponding to the eigenvalue eigenvalues[i]
+	smallest_eigenvector = eigenvectors[:, np.argmin(eigenvalues)]
+	smallest_eigenvector.shape = (3, 4) # shape m matrix into 3 x 4
 	
+	return smallest_eigenvector
 	
 def main():
 	# 1: Caliberation
@@ -42,9 +43,21 @@ def main():
 	# Read in normalized 2d points
 	points_2d_norm = np.loadtxt('input/pts2d-norm-pic_a.txt', delimiter = ' ')
 	points_3d_norm = np.loadtxt('input/pts3d-norm.txt', delimiter = ' ')
-	svd(points_2d_norm, points_3d_norm)
-	
-		
+	points_3d_norm_len = len(points_3d_norm)
+	M = svd(points_2d_norm, points_3d_norm)
+	# [u, v] projection of the last point
+	point_3d_last = points_3d_norm[points_3d_norm_len - 1] # [x, y, z]
+	point_3d_last = np.append(point_3d_last, 1) # [x, y, z, 1]
+	point_3d_last.shape = (4, 1)
+	print "\nM: \n"
+	print M
+	print "\n3D Point: \n"
+	print point_3d_last
+	point_2d_calculated = np.matmul(M, point_3d_last)
+	point_2d_calculated = point_2d_calculated / point_2d_calculated[2]
+	point_2d_calculated = point_2d_calculated[0:2]
+	print "\n2D Point: \n"
+	print point_2d_calculated
 	
 	
 if __name__ == "__main__":
