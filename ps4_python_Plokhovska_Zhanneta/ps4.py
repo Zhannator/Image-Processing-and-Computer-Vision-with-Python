4c0e40d683e8ddd9c19e68cf9a502482cdc5b745
Zhannator <zhp3@pitt.edu>
2018-11-04 19:35:28 -0500

    Initialized folder for ps4

---
+++ ps4_python_Plokhovska_Zhanneta/ps4.py
@@ -0,0 +1,16 @@
+# Public libraries
+import sys
+import os
+import numpy as np
+import cv2
+import math
+import random
+
+def main():
+	
+	# 1-A: 
+	print "\n-----------------------1-A-----------------------" 
+
+	
+if __name__ == "__main__":
+	main()
