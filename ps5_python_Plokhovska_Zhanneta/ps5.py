bcb1aa0950cda57c3a8de44d9f0e94800e30515d
Zhanneta Plokhovska <zhp3@pitt.edu>
2018-11-28 12:41:57 -0500

    2a

--- ps5_python_Plokhovska_Zhanneta/ps5.py
+++ ps5_python_Plokhovska_Zhanneta/ps5.py
@@ -77,65 +77,96 @@ def normalize(img, distribute = True):
 		img = (img * (1/img.max()) * 255) # distribute values between 0 and 255
 	return img.astype('uint8')
 
-def main():
-	# 1-A: LK optic flow estimation
-	print "\n-----------------------1-A-----------------------" 
-	# Read images
-	shift0 = cv2.imread(os.path.join('input/TestSeq', 'Shift0.png'), 0) #* (1.0 / 255.0) # grayscale
-	shiftr2 = cv2.imread(os.path.join('input/TestSeq', 'ShiftR2.png'), 0) #* (1.0 / 255.0)  # grayscale
-	shiftr5u5 = cv2.imread(os.path.join('input/TestSeq', 'ShiftR5U5.png'), 0) #* (1.0 / 255.0)  # grayscale
-	shiftr10 = cv2.imread(os.path.join('input/TestSeq', 'ShiftR10.png'), 0) #* (1.0 / 255.0)  # grayscale
-	shiftr20 = cv2.imread(os.path.join('input/TestSeq', 'ShiftR20.png'), 0) #* (1.0 / 255.0)  # grayscale
-	shiftr40 = cv2.imread(os.path.join('input/TestSeq', 'ShiftR40.png'), 0) #* (1.0 / 255.0)  # grayscale
-	# Smooth images
-	shift0 = cv2.GaussianBlur(shift0, (3, 3), 0)
-	shiftr2 = cv2.GaussianBlur(shiftr2, (3, 3), 0)
-	shiftr5u5 = cv2.GaussianBlur(shiftr5u5, (3, 3), 0)
-	shiftr10 = cv2.GaussianBlur(shiftr10, (3, 3), 0)
-	shiftr20 = cv2.GaussianBlur(shiftr20, (3, 3), 0)
-	shiftr40 = cv2.GaussianBlur(shiftr40, (3, 3), 0)
-	# Get image gradients
-	shift0_Ix, shift0_Iy = compute_gradient(shift0)
-	# Get image difference
-	shift0_shiftr2_It = shiftr2 - shift0
-	shift0_shiftr5u5_It = shiftr5u5 - shift0
-	# Calculate displacement images using LK optic flow estimation
-	shift0_shiftr2_u, shift0_shiftr2_v = lk_optic_flow(shift0_Ix, shift0_Iy, shift0_shiftr2_It)
-	shift0_shiftr5u5_u, shift0_shiftr5u5_v = lk_optic_flow(shift0_Ix, shift0_Iy, shift0_shiftr5u5_It)
-	# Save images
-	shift0_shiftr2 = cv2.hconcat([normalize(shift0_shiftr2_u, False), normalize(shift0_shiftr2_v, False)])
-	plt.imshow(shift0_shiftr2);
-	plt.colorbar()
-	plt.savefig('output/ps5-1-a-1.png')
-	shift0_shiftr5u5 = cv2.hconcat([normalize(shift0_shiftr5u5_u, False), normalize(shift0_shiftr5u5_v, False)])
-	plt.imshow(shift0_shiftr5u5);
-	plt.savefig('output/ps5-1-a-2.png')
+def reduce(img):
+	return img[::2, 1::2]
 	
-	# 1-B: LK optic flow estimation
-	print "\n-----------------------1-B-----------------------" 
-	# Get image difference
-	shift0_shiftr10_It = shiftr10 - shift0
-	shift0_shiftr20_It = shiftr20 - shift0
-	shift0_shiftr40_It = shiftr40 - shift0
-	# Calculate displacement images using LK optic flow estimation
-	shift0_shiftr10_u, shift0_shiftr10_v = lk_optic_flow(shift0_Ix, shift0_Iy, shift0_shiftr10_It)
-	shift0_shiftr20_u, shift0_shiftr20_v = lk_optic_flow(shift0_Ix, shift0_Iy, shift0_shiftr20_It)
-	shift0_shiftr40_u, shift0_shiftr40_v = lk_optic_flow(shift0_Ix, shift0_Iy, shift0_shiftr40_It)
-	# Save images
-	shift0_shiftr10 = cv2.hconcat([normalize(shift0_shiftr10_u, False), normalize(shift0_shiftr10_v, False)])
-	plt.imshow(shift0_shiftr10);
-	plt.colorbar()
-	plt.savefig('output/ps5-1-b-1.png')
-	shift0_shiftr20 = cv2.hconcat([normalize(shift0_shiftr20_u, False), normalize(shift0_shiftr20_v, False)])
-	plt.imshow(shift0_shiftr20);
-	plt.savefig('output/ps5-1-b-2.png')
-	shift0_shiftr40 = cv2.hconcat([normalize(shift0_shiftr40_u, False), normalize(shift0_shiftr40_v, False)])
-	plt.imshow(shift0_shiftr40);
-	plt.savefig('output/ps5-1-b-3.png')
+def main():
+	if False:
+		# 1-A: LK optic flow estimation
+		print "\n-----------------------1-A-----------------------" 
+		# Read images
+		shift0 = cv2.imread(os.path.join('input/TestSeq', 'Shift0.png'), 0)
+		shiftr2 = cv2.imread(os.path.join('input/TestSeq', 'ShiftR2.png'), 0)
+		shiftr5u5 = cv2.imread(os.path.join('input/TestSeq', 'ShiftR5U5.png'), 0)
+		shiftr10 = cv2.imread(os.path.join('input/TestSeq', 'ShiftR10.png'), 0)
+		shiftr20 = cv2.imread(os.path.join('input/TestSeq', 'ShiftR20.png'), 0)
+		shiftr40 = cv2.imread(os.path.join('input/TestSeq', 'ShiftR40.png'), 0)
+		# Smooth images
+		shift0 = cv2.GaussianBlur(shift0, (5, 5), 0)
+		shiftr2 = cv2.GaussianBlur(shiftr2, (5, 5), 0)
+		shiftr5u5 = cv2.GaussianBlur(shiftr5u5, (5, 5), 0)
+		shiftr10 = cv2.GaussianBlur(shiftr10, (5, 5), 0)
+		shiftr20 = cv2.GaussianBlur(shiftr20, (5, 5), 0)
+		shiftr40 = cv2.GaussianBlur(shiftr40, (5, 5), 0)
+		# Get image gradients
+		shift0_Ix, shift0_Iy = compute_gradient(shift0)
+		# Get image difference
+		shift0_shiftr2_It = shiftr2 - shift0
+		shift0_shiftr5u5_It = shiftr5u5 - shift0
+		# Calculate displacement images using LK optic flow estimation
+		shift0_shiftr2_u, shift0_shiftr2_v = lk_optic_flow(shift0_Ix, shift0_Iy, shift0_shiftr2_It)
+		shift0_shiftr5u5_u, shift0_shiftr5u5_v = lk_optic_flow(shift0_Ix, shift0_Iy, shift0_shiftr5u5_It)
+		# Save images
+		shift0_shiftr2 = cv2.hconcat([normalize(shift0_shiftr2_u, False), normalize(shift0_shiftr2_v, False)])
+		plt.imshow(shift0_shiftr2);
+		plt.colorbar()
+		plt.savefig('output/ps5-1-a-1.png')
+		shift0_shiftr5u5 = cv2.hconcat([normalize(shift0_shiftr5u5_u, False), normalize(shift0_shiftr5u5_v, False)])
+		plt.imshow(shift0_shiftr5u5);
+		plt.savefig('output/ps5-1-a-2.png')
+		
+		# 1-B: LK optic flow estimation
+		print "\n-----------------------1-B-----------------------" 
+		# Get image difference
+		shift0_shiftr10_It = shiftr10 - shift0
+		shift0_shiftr20_It = shiftr20 - shift0
+		shift0_shiftr40_It = shiftr40 - shift0
+		# Calculate displacement images using LK optic flow estimation
+		shift0_shiftr10_u, shift0_shiftr10_v = lk_optic_flow(shift0_Ix, shift0_Iy, shift0_shiftr10_It)
+		shift0_shiftr20_u, shift0_shiftr20_v = lk_optic_flow(shift0_Ix, shift0_Iy, shift0_shiftr20_It)
+		shift0_shiftr40_u, shift0_shiftr40_v = lk_optic_flow(shift0_Ix, shift0_Iy, shift0_shiftr40_It)
+		# Save images
+		shift0_shiftr10 = cv2.hconcat([normalize(shift0_shiftr10_u, False), normalize(shift0_shiftr10_v, False)])
+		plt.imshow(shift0_shiftr10);
+		plt.colorbar()
+		plt.savefig('output/ps5-1-b-1.png')
+		shift0_shiftr20 = cv2.hconcat([normalize(shift0_shiftr20_u, False), normalize(shift0_shiftr20_v, False)])
+		plt.imshow(shift0_shiftr20);
+		plt.savefig('output/ps5-1-b-2.png')
+		shift0_shiftr40 = cv2.hconcat([normalize(shift0_shiftr40_u, False), normalize(shift0_shiftr40_v, False)])
+		plt.imshow(shift0_shiftr40);
+		plt.savefig('output/ps5-1-b-3.png')
 	
 	# 2-A: Gaussian and Laplacian Pyramids
-	print "\n-----------------------1-B-----------------------" 
-	
+	print "\n-----------------------2-A-----------------------" 
+	# Read images
+	yos1 = cv2.imread(os.path.join('input/DataSeq1', 'yos_img_01.jpg'), 0)
+	yos2 = cv2.imread(os.path.join('input/DataSeq1', 'yos_img_02.jpg'), 0)
+	yos3 = cv2.imread(os.path.join('input/DataSeq1', 'yos_img_03.jpg'), 0)
+	# Smooth images
+	yos1 = cv2.GaussianBlur(yos1, (5, 5), 0)
+	yos2 = cv2.GaussianBlur(yos2, (5, 5), 0)
+	yos3 = cv2.GaussianBlur(yos3, (5, 5), 0)
+	# Reduce images
+	yos1_reduced1 = reduce(yos1)
+	yos1_reduced2 = reduce(yos1_reduced1)
+	yos1_reduced3 = reduce(yos1_reduced2)
+	yos1_reduced4 = reduce(yos1_reduced3)
+	# Save images
+	fig = plt.figure()
+	plt.subplot(711)
+	plt.imshow(yos1_reduced1);
+	plt.title('1/2')
+	plt.subplot(713)
+	plt.imshow(yos1_reduced2);
+	plt.title('1/4')
+	plt.subplot(715)
+	plt.imshow(yos1_reduced3);
+	plt.title('1/8')
+	plt.subplot(717)
+	plt.imshow(yos1_reduced4);
+	plt.title('1/16')
+	plt.savefig('output/ps5-2-a-1.png')
 	
 if __name__ == "__main__":
 	main()
